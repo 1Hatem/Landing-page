5a7ace9c0b4085cc5d52172b13a97cfe74317825
Hatem Omran <95854598+1Hatem@users.noreply.github.com>
2022-10-25 21:05:25 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -7,14 +7,14 @@ const sections = Array.from(document.querySelectorAll("section"));
 
 //createElement li and link data-nav and menu__link
 
-function createListItems(){
-for(section in sections){
-listItems=document.createElement("li");
-listItems.innerHTML=`<li><a href="#${sections[section].id} data-nav="${sections[section].id}" class="menu__link">${sections[section].dataset.nav}</a>`;
-navList.appendChild(listItems);
+function createListItems() {
+  for (section in sections) {
+    listItems = document.createElement("li");
+    listItems.innerHTML = `<li><a href="#${sections[section].id}" data-nav="${sections[section].id}" class="menu__link">${sections[section].dataset.nav}</a>`;
+    navList.appendChild(listItems);
 
- }
- 
+  }
+
 }
 
 //calling function
@@ -26,37 +26,40 @@ createListItems();
 //put the properties of animation on the page
 
 
-window.onscroll = function() {
+window.onscroll = function () {
 
- document.querySelectorAll("section").forEach(function(active) {
- 
-	if(
-       
+  document.querySelectorAll("section").forEach(function (active) {
+
+    if (
+
       !active.classList.contains("your-active-class")
 
-    ){
+    ) {
 
-    active.classList.add("your-active-class");
+      active.classList.add("your-active-class");
 
-    } else{
-         active.classList.remove("your-active-class");
+    } else {
+      active.classList.remove("your-active-class");
     }
- });
+  });
 };
 
 
-//make function to be when I click a section in navlist going paragraph section
+//make function to be when I click a section in navlist going paragraph section and going smooth
 
 navList.addEventListener("click", (sec) => {
-    sec.preventDefault();
-    if (sec.target.dataset.nav) {
-      document.getElementById(`${sec.target.dataset.nav}`)
-      document.scrollIntoView({ behavior: "smooth" });
-  //determining speed go to page
+  sec.preventDefault();
+  if (sec.target.dataset.nav) {
+    console.log(sec.target.dataset.nav)
+    const mySec = document.getElementById(`${sec.target.dataset.nav}`); 
+    console.log(mySec)
+    mySec.scrollIntoView({ behavior: "smooth" });
+    //determining speed go to page
     setTimeout(() => {
 
-    location.hash = `${sec.target.dataset.nav}`;
-   },250);
+      location.hash = `${sec.target.dataset.nav}`;
+    }, 250);
   }
 });
+
 
